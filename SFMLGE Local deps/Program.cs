ed6fd5bc774104d8a6e0fdb1ff97e18618053642
That Guy <timmy.anderson.aol.com@gmail.com>
2024-02-27 23:39:23 -0500

    Final cleanup for v0.3 template

--- SFMLGE Local deps/Program.cs
+++ SFMLGE Local deps/Program.cs
@@ -10,34 +10,19 @@ namespace SFML_Game_Engine
         static void Main(string[] args)
         {
             bool appOpen = true;
-            App.Closed += (a, args) => { App.Close(); appOpen = false; };
 
+            App.Closed += (a, args) => { App.Close(); appOpen = false; };
             App.SetFramerateLimit(144);
 
             Project mainProject = new Project("Res", App);
             Scene scene = mainProject.CreateSceneAndLoad("Test!");
 
-            GameObject testGo = scene.CreateGameObject("test!");
-
-            testGo.transform.Position = new Vector2(100, 100);
-            testGo.AddComponent(new Sprite2D(new Vector2(25, 25)));
-
-            GameObject secondGo = scene.CreateGameObject("secondGo", testGo);
-            secondGo.AddComponent(new Sprite2D(new Vector2(50, 50)));
-            secondGo.transform.WorldPosition = new Vector2(55, 0);
-
             mainProject.Start();
 
-            float t = 0;
 
             while (appOpen)
             {
                 App.Clear();
-                t += scene.deltaTime;
-
-                secondGo.transform.WorldPosition = new Vector2(0, 0);
-
-                testGo.transform.Position = new Vector2(100, 100 + MathF.Sin(t)*50);
 
                 mainProject.Update();
                 mainProject.Render(App);
