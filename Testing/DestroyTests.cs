abd61fe079aec7af010783cea8b4964024f7699d
That Guy <timmy.anderson.aol.com@gmail.com>
2025-06-29 14:34:42 -0400

    Adds a Destroy tests, for my own sanity

---
+++ Testing/DestroyTests.cs
@@ -0,0 +1,41 @@
+﻿using SFML_GE.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    [TestClass]
+    public class DestroyTests
+    {
+        [TestMethod]
+        public void TestDestroy()
+        {
+            GEWindow app = new GEWindow(new SFML.Window.VideoMode(512, 512), "Destroy Testing");
+            Project newProject = new Project(null, app);
+            Scene testScene = newProject.CreateSceneAndLoad("TestScene");
+
+            app.SetFramerateLimit(60);
+
+            newProject.Start();
+
+            newProject.Update();
+            testScene.CreateGameObject("Test GO");
+            testScene.CreateGameObject("Test GO 2", testScene.GetGameObject("Test GO"));
+            newProject.Update();
+            testScene.GetGameObject("Test GO")!.Destroy();
+            newProject.Update();
+
+            if (testScene.GetGameObject("Test GO") != null)
+            {
+                throw new Exception("Destroyed GameObject was gotten somehow!");
+            }
+
+
+            app.Close();
+            return;
+        }
+    }
+}
